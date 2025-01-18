3d4a196925bb6331d56a518107bf623dac7e3b11
Ahiregithub <ahirenandini354@gmail.com>
2025-01-18 21:28:51 +0530

    video 2 design base board

--- src/App.js
+++ src/App.js
@@ -1,11 +1,95 @@
 
 function App() {
   return (
-    <div className="App">
-    <h1>
-      hello world 
-    </h1>
+    <div className="trello-master">
+      <nav className="navbar app">App Bar</nav>
+      <nav className="navbar board">Board Bar</nav>
+      <div className="board-column">
+        <div className="columns">
+
+        <header>BrainStorm</header>
+        <ul>
+          <li>
+            <img src="https://trello.com/1/cards/5e20e0e72365b93c7a291206/attachments/5e20e0e72365b93c7a291207/previews/5e20e0e72365b93c7a29120f/download/Design.png"/>
+Design & Research
+          </li>
+          <li>Second</li>
+          <li>third</li>
+          <li>Second</li>
+          <li>Second</li>
+          <li>Second</li>
+          <li>Second</li>
+          <li>Second</li>
+          <li>Second</li>
+          
+        </ul>
+        <footer>Add another card</footer>
+        </div>
         
+        <div className="columns">
+        <header>BrainStorm</header>
+        <ul>
+          <li>
+            <img src="https://trello.com/1/cards/5e20e0e72365b93c7a291206/attachments/5e20e0e72365b93c7a291207/previews/5e20e0e72365b93c7a29120f/download/Design.png"/>
+Design & Research
+          </li>
+          <li>Second</li>
+
+        </ul>
+        <footer>Add another card</footer>
+        </div>
+        <div className="columns">
+        <header>BrainStorm</header>
+        <ul>
+          <li>
+            <img src="https://trello.com/1/cards/5e20e0e72365b93c7a291206/attachments/5e20e0e72365b93c7a291207/previews/5e20e0e72365b93c7a29120f/download/Design.png"/>
+Design & Research
+          </li>
+          <li>Second</li>
+
+        </ul>
+        <footer>Add another card</footer>
+        </div>
+        <div className="columns">
+        <header>BrainStorm</header>
+        <ul>
+          <li>
+            <img src="https://trello.com/1/cards/5e20e0e72365b93c7a291206/attachments/5e20e0e72365b93c7a291207/previews/5e20e0e72365b93c7a29120f/download/Design.png"/>
+Design & Research
+          </li>
+          <li>Second</li>
+
+        </ul>
+        <footer>Add another card</footer>
+        </div>
+        <div className="columns">
+        <header>BrainStorm</header>
+        <ul>
+          <li>
+            <img src="https://trello.com/1/cards/5e20e0e72365b93c7a291206/attachments/5e20e0e72365b93c7a291207/previews/5e20e0e72365b93c7a29120f/download/Design.png"/>
+Design & Research
+          </li>
+          <li>Second</li>
+
+        </ul>
+        <footer>Add another card</footer>
+        </div>
+        <div className="columns">
+        <header>BrainStorm</header>
+        <ul>
+          <li>
+            <img src="https://trello.com/1/cards/5e20e0e72365b93c7a291206/attachments/5e20e0e72365b93c7a291207/previews/5e20e0e72365b93c7a29120f/download/Design.png"/>
+Design & Research
+          </li>
+          <li>Second</li>
+
+        </ul>
+        <footer>Add another card</footer>
+        </div>
+        
+
+      </div>
+
     </div>
   );
 }
